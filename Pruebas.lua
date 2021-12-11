cd74420a842df02cfda580b44ba6ea9b8b273c7b
Arrazer <60563812+Arrazer@users.noreply.github.com>
2021-12-11 17:20:55 -0600

    Update Pruebas.lua

--- Pruebas.lua
+++ Pruebas.lua
@@ -84,30 +84,6 @@ local location = CFrame.new(35, 398, -280)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 pl.CFrame = location
 wait(1)
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(35, 398, -280)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(1)
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(35, 398, -280)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(1)
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(35, 398, -280)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(1)
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(35, 398, -280)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(1)
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(35, 398, -280)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(1)
+
 
 
