900ad4822aabad193e164ae4480faa0365f6f4ff
Arrazer <60563812+Arrazer@users.noreply.github.com>
2021-12-11 18:38:21 -0600

    Update Chapter2.lua

--- Chapter2.lua
+++ Chapter2.lua
@@ -4,4 +4,3 @@ local location = CFrame.new(-1058, 643, -900)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 humanoid:ChangeState(Enum.HumanoidStateType.Flying)
 pl.CFrame = location
-wait(30)
