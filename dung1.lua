4f9ce76277eeb8c0e1b772a6c247ec3e8ac42dbe
Arrazer <60563812+Arrazer@users.noreply.github.com>
2021-12-13 16:51:15 -0600

    Update dung1.lua

--- dung1.lua
+++ dung1.lua
@@ -54,7 +54,7 @@ local location = CFrame.new(-180, 500, 565)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 pl.CFrame = location
   end
-for i=1,23 do
+for i=1,24 do
   quintafase()
   wait(1)
   end
