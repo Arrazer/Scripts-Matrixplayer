1dd0a02131f7b02633c0e21edeac33e7c6cc9a6d
Arrazer <60563812+Arrazer@users.noreply.github.com>
2021-12-14 13:42:17 -0600

    Update dung1.lua

--- dung1.lua
+++ dung1.lua
@@ -20,11 +20,11 @@ local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
 local location = CFrame.new(104, 400, -250)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 pl.CFrame = location
-wait(20)
+wait(10)
 local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
 local location = CFrame.new(-36, 420, -238)
 pl.CFrame = location
-wait(20)
+wait(10)
 --Tercera fase
 function tercerafase()
 local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
@@ -43,7 +43,7 @@ local location = CFrame.new(-190, 408, 220)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 pl.CFrame = location
   end
-for i=1,44 do
+for i=1,22 do
   cuartafase()
   wait(0.5)
   end
