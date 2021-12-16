3e7bd0d51d76fa0071beb50400f37e64db256dbd
Arrazer <60563812+Arrazer@users.noreply.github.com>
2021-12-16 03:21:55 -0600

    Update dung1.lua

--- dung1.lua
+++ dung1.lua
@@ -1,206 +1,72 @@
-    local ToFly = "y" 
-
-repeat wait() 
-    until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid") 
-local mouse = game.Players.LocalPlayer:GetMouse() 
-repeat wait() until mouse
-local plr = game.Players.LocalPlayer 
-local torso = plr.Character.Head 
-local flying = false
-local deb = true 
-local ctrl = {f = 0, b = 0, l = 0, r = 0} 
-local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-local maxspeed = 50 
-local speed = 0 
-
-function Fly() 
-local bg = Instance.new("BodyGyro", torso) 
-bg.P = 9e4 
-bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
-bg.cframe = torso.CFrame 
-local bv = Instance.new("BodyVelocity", torso) 
-bv.velocity = Vector3.new(0,0.1,0) 
-bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
-repeat wait() 
-plr.Character.Humanoid.PlatformStand = true 
-if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
-speed = speed+.5+(speed/maxspeed) 
-if speed > maxspeed then 
-speed = maxspeed 
-end 
-elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
-speed = speed-1 
-if speed < 0 then 
-speed = 0 
-end 
-end 
-if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
-bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
-elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
-bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-else 
-bv.velocity = Vector3.new(0,0.1,0) 
-end 
-bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
-until not flying 
-ctrl = {f = 0, b = 0, l = 0, r = 0} 
-lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-speed = 0 
-bg:Destroy() 
-bv:Destroy() 
-plr.Character.Humanoid.PlatformStand = false 
-end 
-mouse.KeyDown:connect(function(key) 
-if key:lower() == ToFly then 
-if flying then flying = false 
-else 
-flying = true 
-Fly() 
-end 
-elseif key:lower() == "w" then 
-ctrl.f = 1 
-elseif key:lower() == "s" then 
-ctrl.b = -1 
-elseif key:lower() == "a" then 
-ctrl.l = -1 
-elseif key:lower() == "d" then 
-ctrl.r = 1 
-end 
-end) 
-mouse.KeyUp:connect(function(key) 
-if key:lower() == "w" then 
-ctrl.f = 0 
-elseif key:lower() == "s" then 
-ctrl.b = 0 
-elseif key:lower() == "a" then 
-ctrl.l = 0 
-elseif key:lower() == "d" then 
-ctrl.r = 0 
-end 
-end)
-Fly()
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "Y", false, game)
-    wait(.6)
-    function macros()
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "P", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "G", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "F", false, game)
-    wait(.6)
-    vim:SendKeyEvent(true, "U", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "L", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "Q", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "M", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "N", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "J", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "H", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "K", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "B", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "B", false, game)
-    wait(.6)
-    local vim = game:service'VirtualInputManager'
-    vim:SendKeyEvent(true, "E", false, game)
-    wait(.6)
-end
-delay(11, function()
-macros()
-end)
--- Spawn
+--spawn
 local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
 local location = CFrame.new(2863, 3, 1037)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
-humanoid:ChangeState(Enum.HumanoidStateType.Flying)
 pl.CFrame = location
 wait(10)
-delay(19, function()
-macros()
-wait(5)
-macros()
+--Primera fase
+function primerafase()
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(380, 350, -220)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+pl.CFrame = location
+wait(0.1)
+  end
+for i=1,85 do
+  primerafase()
+  end
+--segunda fase
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(104, 400, -250)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+pl.CFrame = location
+wait(10)
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-36, 420, -238)
+pl.CFrame = location
+wait(10)
+--Tercera fase
+function tercerafase()
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-170, 363, -230)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+pl.CFrame = location
+wait(0.1)
+  end
+for i=1,180 do
+  tercerafase()
+  end
+--cuarta fase
+function cuartafase()
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-190, 408, 220)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+pl.CFrame = location
+  end
+for i=1,44 do
+  cuartafase()
+  wait(0.1)
+  end
+--Quinta fase
+function quintafase()
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-180, 500, 565)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+pl.CFrame = location
+  end
+for i=1,10 do
+  quintafase()
+  wait(1)
+  end
 
-end)
---Primera etapa
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(380, 380, -225)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-humanoid:ChangeState(Enum.HumanoidStateType.Flying)
-pl.CFrame = location
-wait(17)
-delay(34, function()
-macros()
-wait(5)
-macros()
-end)
--- Segunda etapa
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(163, 389, -293)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(13)
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(-20, 415, -289)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(20)
-delay(30, function()
-macros()
-end)
--- tercera etapa
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(-151, 400, -307)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(13)
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(-222, 399, -199)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-pl.CFrame = location
-wait(13)
-delay(19, function()
-macros()
-end)
---Cuarta etapa
-local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(-195, 444, 210)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-humanoid:ChangeState(Enum.HumanoidStateType.Flying)
-pl.CFrame = location
-wait(17)
---quinta etapa
-local pl       = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(-260, 475, 510)
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-humanoid:ChangeState(Enum.HumanoidStateType.Flying)
-pl.CFrame = location
-wait(15)
---ainz
+--Boss
+function Ainz()
 local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
 local location = CFrame.new(-285, 447, 770)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 pl.CFrame = location
-while wait(.5) do
-    macros()
-end
+  wait(0.2)
+  end
+for i=1,300 do
+  Ainz()
+  end
