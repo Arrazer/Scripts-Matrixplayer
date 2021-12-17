0d4e675d0359bd0df32deddd2b41eb5631f08174
Arrazer <60563812+Arrazer@users.noreply.github.com>
2021-12-17 02:25:35 -0600

    Update dungeon2_chapter2.lua

--- dungeon2_chapter2.lua
+++ dungeon2_chapter2.lua
@@ -207,8 +207,8 @@ local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
 local location = CFrame.new(-285, 447, 770)
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 pl.CFrame = location
-for(57) do
+for i=1, 57 do
     macros()
 end
-
+game:GetService("TeleportService"):Teleport(7220986579, LocalPlayer)
 
