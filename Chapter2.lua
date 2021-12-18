eb79cdb46902f43782428d2b1817033d07b8fdb3
Arrazer <60563812+Arrazer@users.noreply.github.com>
2021-12-18 02:57:51 -0600

    Update Chapter2.lua

--- Chapter2.lua
+++ Chapter2.lua
@@ -1,4 +1,8 @@
 -- chapter2
 
-game:GetService("TeleportService"):Teleport(7220986579, LocalPlayer)
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-1058, 643, -900)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
 
